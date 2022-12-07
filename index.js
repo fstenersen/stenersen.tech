ec1fcde308e862faee6a64814d6e5dbe3ca6dd0c
Fredrik Leonard Stenersen <fredrlst@stud.ntnu.no>
2022-12-07 17:31:52 +0100

    mobile friendly

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 var dag = new Date();
-var time = now.gethours()
-console.log(dag);
-console.log(time);
+var time = dag.getHours();
+
+console.log("Klokken er " + time);
+
+let i = 1;
+for (i; i < 100; i++) {
+    console.log("Test nr. " + i);
+    if (i % 2 == 0) {
+        console.log("   " + i + " er et partall!");
+    }
+}
+
+let hotel = {
+name: "Radisson Blu",
+rooms: 40,
+pool: true,
+booked: 25,
+checkAvailability:
+    function() {
+       return (this.rooms - this.booked)
+    }
+}
+
+console.log("Det er " + hotel.checkAvailability() + " ledige plasser!");
